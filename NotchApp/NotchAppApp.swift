5c85c9e1b64844b6e5a01ee77f947106f0e55e46
Puneeth N <puneethnarendrababu@gmail.com>
2026-05-09 22:28:14 +0530

    Initial Commit

---
+++ NotchApp/NotchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchAppApp.swift
+//  NotchApp
+//
+//  Created by Puneeth N on 09/05/26.
+//
+
+import SwiftUI
+
+@main
+struct NotchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
